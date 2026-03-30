99bc078f4ed6ba1f3e6f2adee962793885b8b06c
SHO800 <fg582323@docomo.ne.jp>
2026-03-30 20:19:11 +0900

    pages siteになってしまったのでbasePathを設定

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+
 const nextConfig = {
   /* config options here */
     transpilePackages: ['three'],
     
     output: "export",
+    basePath: basePath,
+    assetPrefix: basePath,
+    
     images: {
         // 静的エクスポートではNext.jsの画像最適化APIが使えないため無効化
         unoptimized: true,
